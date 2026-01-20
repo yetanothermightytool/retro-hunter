b947dff4d97bc0c9f5f5ababac535f15c4d6be4b
YAMT <94518822+yetanothermightytool@users.noreply.github.com>
2026-01-20 11:20:16 +0100

    v3.0

---
+++ create-fernet-files.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from cryptography.fernet import Fernet
+import getpass
+
+def generate_key():
+   key = Fernet.generate_key()
+   with open("encryption_key.key", "wb") as key_file:
+       key_file.write(key)
+   return key
+
+def load_key():
+   try:
+       with open("encryption_key.key", "rb") as key_file:
+           key = key_file.read()
+   except FileNotFoundError:
+       print("❗ Encryption key not found. Generating a new key...")
+       key = generate_key()
+   return key
+
+def encrypt_password(password, output_file, key):
+   fernet = Fernet(key)
+   encrypted_password = fernet.encrypt(password.encode())
+   with open(output_file, "wb") as encrypted_file:
+       encrypted_file.write(encrypted_password)
+   print(f"🔒 Password encrypted and stored securely in {output_file}.")
+
+if __name__ == "__main__":
+   key = load_key()
+
+   rest_password = getpass.getpass("🔑 Enter the Veeam REST API user password to be stored securely: ")
+   encrypt_password(rest_password, "encrypted_password.bin", key)
+
+   smb_password = getpass.getpass("🔑 Enter the SMB user password to be stored securely: ")
+   encrypt_password(smb_password, "encrypted_smb_password.bin", key)
